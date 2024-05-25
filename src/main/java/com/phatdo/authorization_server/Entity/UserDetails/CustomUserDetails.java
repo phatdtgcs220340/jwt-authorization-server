d24e6b01e4af9238cfb056c4e369caaa5bcd36fc
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-05-25 12:18:41 +0700

    custom user details

---
+++ src/main/java/com/phatdo/authorization_server/Entity/UserDetails/CustomUserDetails.java
@@ -0,0 +1,36 @@
+package com.phatdo.authorization_server.Entity.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.phatdo.authorization_server.Entity.User.User;
+
+public class CustomUserDetails implements UserDetails {
+    private User user;
+
+    @Autowired
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+}
---
+++ src/main/java/com/phatdo/authorization_server/Entity/UserDetails/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.phatdo.authorization_server.Entity.UserDetails;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.phatdo.authorization_server.Entity.User.User;
+import com.phatdo.authorization_server.Entity.User.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return new CustomUserDetails(user);
+    }
+
+}
