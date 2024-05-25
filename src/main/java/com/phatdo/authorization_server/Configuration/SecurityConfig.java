6cb688c35b587705acd5329c43a32f0b3e500bfc
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-05-25 13:09:08 +0700

    general security config

---
+++ src/main/java/com/phatdo/authorization_server/Configuration/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.phatdo.authorization_server.Configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.phatdo.authorization_server.Entity.User.UserRepository;
+import com.phatdo.authorization_server.Entity.UserDetails.CustomUserDetailsService;
+
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(userRepository, passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultfilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(c -> c.anyRequest().authenticated())
+                .logout(r -> r
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("http://localhost:8080/logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID"));
+        return http.build();
+    }
+}
