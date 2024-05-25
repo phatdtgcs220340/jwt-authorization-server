4d2b02f5f2b5a9959e8df6a49c87c5245b84b7c8
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-05-25 14:29:08 +0700

    fix add custom claim bug

--- src/main/java/com/phatdo/authorization_server/Configuration/OAuth2/OAuth2CustomToken.java
+++ src/main/java/com/phatdo/authorization_server/Configuration/OAuth2/OAuth2CustomToken.java
@@ -1,22 +1,29 @@
 package com.phatdo.authorization_server.Configuration.OAuth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
+import com.phatdo.authorization_server.Entity.User.User;
 import com.phatdo.authorization_server.Entity.UserDetails.CustomUserDetails;
+import com.phatdo.authorization_server.Entity.UserDetails.CustomUserDetailsService;
 
 @Configuration
 public class OAuth2CustomToken {
+    @Autowired
+    private CustomUserDetailsService userService;
+
     @Bean
     public OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
         return context -> {
             context.getClaims().claims((claims) -> {
-                CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext()
-                        .getAuthentication().getPrincipal();
-                String fullname = userDetails.getUser().getFullName();
+                String username = context.getPrincipal().getName();
+                User user = ((CustomUserDetails) userService
+                        .loadUserByUsername(username))
+                        .getUser();
+                String fullname = user.getFullName();
                 claims.put("fullName", fullname);
             });
         };
