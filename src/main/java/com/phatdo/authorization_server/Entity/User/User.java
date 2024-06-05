091204cb690ce6f4b577f2a443a9532a7aa20803
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-06-05 13:26:14 +0700

    add spring validation

--- src/main/java/com/phatdo/authorization_server/Entity/User/User.java
+++ src/main/java/com/phatdo/authorization_server/Entity/User/User.java
@@ -1,6 +1,9 @@
 package com.phatdo.authorization_server.Entity.User;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,10 +19,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private int userId;
+
+    @NotBlank
     @Column(name = "full_name")
     private final String fullName;
+
+    @Email
     @Column(name = "username", unique = true)
     private final String username;
+
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     @Column(name = "password")
     private final String password;
 }
