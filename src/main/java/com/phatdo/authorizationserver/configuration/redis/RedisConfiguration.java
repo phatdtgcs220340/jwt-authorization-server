ea22dbe18233adb0bf3c4d8e367aaaaf650d6e50
lãng tử cam ranh <114555261+phatdtgcs220340@users.noreply.github.com>
2024-08-01 01:53:15 +0700

    redis base config

---
+++ src/main/java/com/phatdo/authorizationserver/configuration/redis/RedisConfiguration.java
@@ -0,0 +1,25 @@
+package com.phatdo.authorizationserver.configuration.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfiguration {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
